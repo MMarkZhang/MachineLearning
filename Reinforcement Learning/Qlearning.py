ff8a63aee86ce819c023b4918805efe924fbb9e9
MMarkZhang <mark@wireless-10-145-48-100.public.utexas.edu>
2020-01-28 16:24:25 -0600

    update RL

--- Reinforcement Learning/Qlearning.py
+++ Reinforcement Learning/Qlearning.py
@@ -25,5 +25,82 @@ def build_q_table(n_states, actions):
     )
     return table
 
-t = build_q_table(N_STATES, ACTIONS)
-print(t)
+#t = build_q_table(N_STATES, ACTIONS)
+#print(t)
+    
+def choose_action(state, q_table):
+    state_actions = q_table.iloc[state, :]
+    if(np.random.uniform() > EPSILON) or (state_actions.all() == 0):
+        # if all 0 -> randomly choose action 
+        action_name = np.random.choice(ACTIONS)
+    else:
+        # choose the best option 
+        action_name = state_actions.argmax()
+    return action_name
+
+def get_env_feedback(S, A):
+    # This is how agent will interact with the environment
+    if A == 'right':    # move right
+        if S == N_STATES - 2:   # terminate (reach the goal)
+            S_ = 'terminal'
+            R = 1 # reward
+        else:
+            S_ = S + 1
+            R = 0
+    else:   # move left
+        R = 0
+        if S == 0:
+            S_ = S  # reach the wall
+        else:
+            S_ = S - 1
+    return S_, R
+
+def update_env(S, episode, step_counter): #setting up enviornment 
+    # This is how environment be updated
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(2)
+        print('\r                                ', end='')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(FRESH_TIME)
+
+
+def rl():
+    q_table = build_q_table(N_STATES, ACTIONS)  # initialize q table
+    for episode in range(MAX_EPISODES):     
+        step_counter = 0
+        S = 0   # initialize at position 0, the left wall
+        is_terminated = False   # terminate or not
+        update_env(S, episode, step_counter)    # update enviorn
+        while not is_terminated:
+            
+            A = choose_action(S, q_table)   # choose action
+            S_, R = get_env_feedback(S, A)  # whether we get reward
+            q_predict = q_table.loc[S, A]    # predicted value
+            if S_ != 'terminal': # did not terminated
+                q_target = R + GAMMA * q_table.iloc[S_, :].max()   #  real value
+            else: #terminated
+                q_target = R     #  get reward
+                is_terminated = True    # terminate this episode
+
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)  #  update q-table
+            S = S_  # next state
+
+            update_env(S, episode, step_counter+1)  # update
+
+            step_counter += 1
+    return q_table
+
+if __name__ == "__main__":
+    q_table = rl()
+    print('\r\nQ-table:\n')
+    print(q_table)
+
+
+
+
